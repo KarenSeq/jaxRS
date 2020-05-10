183b4f3b0988fbc128720b80424ae4da2d4bc9b8
Karen Sequeira <karen.sequeira@ad.infosys.com>
2020-05-10 12:09:39 +0530

    rest jax rs project

---
+++ src/org/learn/jaxrs/Book.java
@@ -0,0 +1,36 @@
+package org.learn.jaxrs;
+
+public class Book {
+	private String name;
+	//@XmlElement(name = "author")
+	private String author;
+
+	public Book() {
+
+	}
+	/*
+	 * public Book(String name, String author) { this.name = name; this.author =
+	 * author; }
+	 */
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", author=" + author + "]";
+	}
+}
